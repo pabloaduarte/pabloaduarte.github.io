42c9446ab38bd5161a83d412d74a8ab9a1568c95
pabloaduarte <pablo.duarte@gmail.com>
2022-03-16 18:13:04 +0100

    Fixed Russian Charts

---
+++ git_setup.R
@@ -0,0 +1,6 @@
+library(usethis)
+edit_git_config()
+
+usethis::use_git_config(user.name = "pabloaduarte", user.email = "pablo.duarte@gmail.com")
+credentials::set_github_pat("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")
+credentials::set_github_pat("my_pat")
